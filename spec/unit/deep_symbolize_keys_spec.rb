80c06eeab59f8803820485a7f03b9656565a3b35
Gareth Rushgrove <gareth@morethanseven.net>
2016-07-29 17:56:22 +0100

    Getting going with adding tests

---
+++ spec/unit/deep_symbolize_keys_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'puppet_x/puppetlabs/deep_symbolize_keys'
+
+describe 'deep_symbolize_keys' do
+  describe Hash do
+    it 'should have been monkey patched with deep_symbolize_keys' do
+      expect(subject.respond_to? :deep_symbolize_keys)
+    end
+    it 'should have symbols for keys after deep_symbolize_keys is called' do
+      expect({'a': 1}.deep_symbolize_keys).to eq({a: 1})
+    end
+  end
+
+  describe Array do
+    it 'should have been monkey patched with deep_symbolize_keys' do
+      expect(subject.respond_to? :deep_symbolize_keys)
+    end
+    it 'should have symbols for keys after deep_symbolize_keys is called' do
+      expect([{'a': 1}].deep_symbolize_keys).to eq([{a: 1}])
+    end
+  end
+end
--- spec/unit/dockerfile_spec.rb
+++ spec/unit/dockerfile_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 require 'puppet_x/puppetlabs/dockerfile'
 
 describe PuppetX::Puppetlabs::Dockerfile do
+  before(:all) do
+    @dockerfile = PuppetX::Puppetlabs::Dockerfile.new
+  end
+
+  context '#render' do
+    it 'should return a string' do
+      expect(@dockerfile.render).to be_a(String)
+    end
+  end
+
+  context '#save' do
+    it 'should return a file handle' do
+      expect(@dockerfile.save).to be_a_kind_of(Tempfile)
+    end
+  end
 end
