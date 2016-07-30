dcba482ea946fee2fbfb7eef9171e60e515e23e4
Gareth Rushgrove <gareth@morethanseven.net>
2016-07-30 11:53:33 +0100

    Improve coverage for face commands

--- spec/unit/docker_face_spec.rb
+++ spec/unit/docker_face_spec.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 require 'spec_helper'
 require 'puppet/face'
 
@@ -30,6 +32,31 @@ describe Puppet::Face[:docker, '0.1.0'] do
       it 'should fail if not passed a manifest' do
         expect { subject.send(subcommand) }.to raise_exception(ArgumentError, /wrong number of arguments/)
       end
+
+      it 'should fail if passed non existent manifest' do
+        expect { subject.send(subcommand, 'not-a-real-file') }.to raise_exception(RuntimeError, /does not exist/)
+      end
+    end
+  end
+
+  describe "#build specific options" do
+    let(:manifest) { Tempfile.new('manifest.pp') }
+    it 'should catch issues with the underlying build' do
+      expect_any_instance_of(PuppetX::Puppetlabs::DockerImageBuilder).to receive(:build).and_raise(PuppetX::Puppetlabs::BuildError)
+      expect { subject.build(manifest.path, {image_name: 'sample'}) }.to raise_exception(RuntimeError, /the build process was interupted/)
+    end
+
+    it 'should run with the minimum options' do
+      expect_any_instance_of(PuppetX::Puppetlabs::DockerImageBuilder).to receive(:build)
+      expect { subject.build(manifest.path, {image_name: 'sample'}) }.not_to raise_error
+    end
+  end
+
+  describe "#dockerfile specific options" do
+    let(:manifest) { Tempfile.new('manifest.pp') }
+    it 'should run with the minimum options' do
+      expect_any_instance_of(PuppetX::Puppetlabs::Dockerfile).to receive(:render)
+      expect { subject.dockerfile(manifest.path, {image_name: 'sample'}) }.not_to raise_error
     end
   end
 end
