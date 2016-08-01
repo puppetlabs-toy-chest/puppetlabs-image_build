9205a369e461de6811580fbccabb4b5b013584e3
Gareth Rushgrove <gareth@morethanseven.net>
2016-08-01 17:15:27 +0100

    Added RHEL based node to the acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -19,6 +19,10 @@ RSpec.configure do |c|
     puppet_module_install(source: proj_root, module_name: module_name)
     hosts.each do |host|
       BeakerSpecHelper::spec_prep(host)
+      if fact_on(host, 'osfamily') == 'RedHat'
+        on(host, 'sudo yum update -y -q')
+        on(host, 'sudo systemctl stop firewalld')
+      end
 		  on host, puppet('module', 'install', 'garethr-docker'), { :acceptable_exit_codes => [0,1] }
     end
   end
