puppetlabs-toy-chest/puppetlabs-image_build

commit c4e6991126ca0ee9aab5faf178a9a9d23c175992

Updates for latest rubocop style hints

lib/puppet_x/puppetlabs/imagebuilder.rb
@@ -128,11 +128,13 @@ module PuppetX
       end
 
       def add_label_schema_labels
-        @context[:labels].insert(
-          -1,
-          "org.label-schema.build-date=#{Time.now.utc.iso8601}",
-          'org.label-schema.schema-version=1.0'
-        ) if @context[:label_schema]
+        if @context[:label_schema]
+          @context[:labels].insert(
+            -1,
+            "org.label-schema.build-date=#{Time.now.utc.iso8601}",
+            'org.label-schema.schema-version=1.0'
+          )
+        end
       end
 
       def env_to_array
@@ -184,7 +186,7 @@ module PuppetX
                                end
       end
 
-      def determine_environment_vars # rubocop:disable Metrics/AbcSize,Metrics/PerceivedComplexity
+      def determine_environment_vars # rubocop:disable Metrics/AbcSize
         codename = nil
         puppet_version = nil
         facter_version = nil
@@ -207,7 +209,6 @@ module PuppetX
                      when 'wheezy', /^7/
                        'wheezy'
                      end
-        when 'centos'
         when 'alpine'
           facter_version = '2.4.6' # latest version available as a gem
           puppet_version = case @context[:puppet_agent_version]
@@ -236,9 +237,11 @@ module PuppetX
           puppet_version: puppet_version,
           facter_version: facter_version,
         }.reject { |name, value| value.nil? }
-        @context[:env].map { |pair| pair.split('=') }.each do |name, value|
-          @context[:environment][name] = value
-        end unless @context[:env].nil?
+        unless @context[:env].nil?
+          @context[:env].map { |pair| pair.split('=') }.each do |name, value|
+            @context[:environment][name] = value
+          end
+        end
       end
 
       def determine_hostname

lib/puppet/face/aci.rb
@@ -1,7 +1,7 @@
 require 'puppet_x/puppetlabs/imagebuilder'
 require 'puppet_x/puppetlabs/imagebuilder_face'
 
-PuppetX::Puppetlabs::ImageBuilder::Face.define(:aci, '0.1.0') do
+PuppetX::Puppetlabs::ImageBuilder::Face.define(:aci, '0.1.0') do # rubocop:disable Metrics/BlockLength
   summary 'Build Aci images and build scripts using Puppet code'
 
   action(:build) do

lib/puppet/face/docker.rb
@@ -1,7 +1,7 @@
 require 'puppet_x/puppetlabs/imagebuilder'
 require 'puppet_x/puppetlabs/imagebuilder_face'
 
-PuppetX::Puppetlabs::ImageBuilder::Face.define(:docker, '0.1.0') do
+PuppetX::Puppetlabs::ImageBuilder::Face.define(:docker, '0.1.0') do # rubocop:disable Metrics/BlockLength
   summary 'Build Docker images and Dockerfiles using Puppet code'
 
   option '--rocker' do
@@ -9,7 +9,7 @@ PuppetX::Puppetlabs::ImageBuilder::Face.define(:docker, '0.1.0') do
     default_to { false }
   end
 
-  action(:build) do
+  action(:build) do # rubocop:disable Metrics/BlockLength
     summary 'Build a Docker image from Puppet code'
     arguments '[<manifest>]'
     default

lib/puppet_x/puppetlabs/deep_symbolize_keys.rb
@@ -1,11 +1,15 @@
 class Object
   def deep_symbolize_keys
-    return self.reduce({}) do |memo, (k, v)|
-      memo.tap { |m| m[k.to_sym] = v.deep_symbolize_keys }
-    end if self.is_a? Hash
-    return self.reduce([]) do |memo, v|
-      memo << v.deep_symbolize_keys; memo
-    end if self.is_a? Array
+    if self.is_a? Hash
+      return self.reduce({}) do |memo, (k, v)|
+        memo.tap { |m| m[k.to_sym] = v.deep_symbolize_keys }
+      end
+    end
+    if self.is_a? Array
+      return self.reduce([]) do |memo, v|
+        memo << v.deep_symbolize_keys; memo
+      end
+    end
     self
   end
 end

commit d4e7d78032b98b0d2394739754ee080fc4393f98

Fix support for building Centos images
This was broken in a previous commit
c4e6991126ca0ee9aab5faf178a9a9d23c175992

lib/puppet_x/puppetlabs/imagebuilder.rb
@@ -258,6 +258,7 @@ module PuppetX
                            else
                              '4.5.2'
                            end
+        when 'centos' # rubocop:disable Lint/EmptyWhen
         else
           raise InvalidContextError, 'puppet docker currently only supports Ubuntu, Debian, Alpine and Centos base images'
         end

spec/support/examples/imagebuilder.rb
@@ -574,4 +574,16 @@ invalid
       end
     end
   end
+
+  context "when using a centos image" do
+    let(:args) do
+      {
+        from: "centos:7",
+        image_name: image_name,
+      }
+    end
+    it 'should not raise an error' do
+      expect { context }.not_to raise_error
+    end
+  end
 end